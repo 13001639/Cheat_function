6ba956d3dae361870aeaae63c9f24883ebce65f1
13001639 <157706709+13001639@users.noreply.github.com>
2024-01-25 14:02:39 +0100

    Add files via upload

---
+++ My_cheat_function.py
@@ -0,0 +1,7 @@
+def cheat(string):
+    if string == 'Q2.2.4':
+        return ("First, define special vector creating array with vector[0], then with a for loop in range of the length of the vector, if i of vector is not in the special vector, append the i to it.")
+    elif string == 'Q2.2.1':
+        return ("Import datetime, define current hour, minute and time by adding them. if current time is in the range (use if _ and _:), print required message.")
+    else:
+        return ("Sorry, not in my definition")
